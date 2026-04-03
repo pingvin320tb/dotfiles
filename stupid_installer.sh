9f453f8e3aef5a443a26a99d1fa1f3d7992f07e7
pingvin320tb <ondrinsemen@gmail.com>
2026-04-03 18:23:48 +0200

    add very VERY stupid installer

---
+++ stupid_installer.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+ git clone https://github.com/zsh-users/zsh-autosuggestions ~/dotfiles/.oh-my-zsh/custom/plugins/zsh-autosuggestions
+ git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/dotfiles/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
+ git clone https://github.com/Aloxaf/fzf-tab ~/dotfiles/.oh-my-zsh/custom/plugins/fzf-tab
+ git clone https://github.com/romkatv/powerlevel10k.git ~/dotfiles/.oh-my-zsh/custom/themes/powerlevel10k
