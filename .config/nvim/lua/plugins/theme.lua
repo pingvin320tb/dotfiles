ecb95a2a4d1752411fce8e986f5b36ae5bfed741
pingvin320tb <ondrinsemen@gmail.com>
2026-05-08 17:56:18 +0200

    migrate to lazyvim

---
+++ .config/nvim/lua/plugins/theme.lua
@@ -0,0 +1,12 @@
+return {
+  -- gruvbox plugin
+  { "ellisonleao/gruvbox.nvim" },
+
+  -- set theme gruvbox
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "gruvbox",
+    },
+  },
+}
